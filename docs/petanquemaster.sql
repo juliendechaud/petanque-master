34dd2d9624075bf479b71b534a3f1f9c44b5208c
Dulkien <dulkiendev@gmail.com>
2020-04-21 21:57:23 +0200

    update de la DB (non terminé)

--- docs/petanquemaster.sql
+++ docs/petanquemaster.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : localhost
--- Généré le : lun. 20 avr. 2020 à 16:52
+-- Généré le : mar. 21 avr. 2020 à 19:56
 -- Version du serveur :  10.4.12-MariaDB
 -- Version de PHP : 7.4.5
 
@@ -29,6 +29,7 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `articles` (
   `ref` int(11) NOT NULL,
+  `nom` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
   `prix` float NOT NULL,
   `photo` text COLLATE utf8mb4_unicode_ci NOT NULL,
   `description` text COLLATE utf8mb4_unicode_ci NOT NULL,
@@ -39,8 +40,26 @@ CREATE TABLE `articles` (
 -- Déchargement des données de la table `articles`
 --
 
-INSERT INTO `articles` (`ref`, `prix`, `photo`, `description`, `categ`) VALUES
-(1, 12.5, 'img/article/obut-boule.png', 'La best boule', 1);
+INSERT INTO `articles` (`ref`, `nom`, `prix`, `photo`, `description`, `categ`) VALUES
+(1, 'Une Belle Boule', 12.5, 'img/article/obut-boule.png', 'La best boule', 1),
+(2, 'René', 74.12, 'img/article/mrmvHonteux.png', 'Ah oui', 1),
+(3, 'Zizou?', 1.2, 'img/article/mrmvZizou.png', 'Jadore la vie', 2),
+(4, 'La Boule Magique', 48.75, 'img/article/mrmvBM.png', 'Connaissez-vous René Tech Prime ?', 1),
+(8, 'Pastis 51', 20, 'img/article/pastis51.png', 'PASTIS 51 est une boisson aux origines chaleureuses et ensoleillées, composée de délicats arômes naturels d\'anis pour une incomparable fraicheur. Un grand classique aux doux parfums du Sud.', 3);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `avis`
+--
+
+CREATE TABLE `avis` (
+  `idavis` int(11) NOT NULL,
+  `commentaire` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `note` int(11) NOT NULL,
+  `idutilisateur` int(11) NOT NULL,
+  `refarticle` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -58,7 +77,48 @@ CREATE TABLE `categorie` (
 --
 
 INSERT INTO `categorie` (`idcateg`, `libelle`) VALUES
-(1, 'Boules');
+(1, 'Boules'),
+(2, 'Accessoire'),
+(3, 'Boisson');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `commande`
+--
+
+CREATE TABLE `commande` (
+  `idcom` int(11) NOT NULL,
+  `datecom` date NOT NULL,
+  `idutili` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `composition`
+--
+
+CREATE TABLE `composition` (
+  `idcom` int(11) NOT NULL,
+  `refarticle` int(11) NOT NULL,
+  `qte` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `utilisateur`
+--
+
+CREATE TABLE `utilisateur` (
+  `idu` int(11) NOT NULL,
+  `nom` varchar(250) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `prenom` varchar(250) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `mail` varchar(250) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `adresse` varchar(300) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `mdp` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Index pour les tables déchargées
@@ -72,10 +132,34 @@ ALTER TABLE `articles`
   ADD KEY `categ` (`categ`);
 
 --
+-- Index pour la table `avis`
+--
+ALTER TABLE `avis`
+  ADD PRIMARY KEY (`idavis`);
+
+--
 -- Index pour la table `categorie`
 --
 ALTER TABLE `categorie`
   ADD PRIMARY KEY (`idcateg`);
+
+--
+-- Index pour la table `commande`
+--
+ALTER TABLE `commande`
+  ADD PRIMARY KEY (`idcom`);
+
+--
+-- Index pour la table `composition`
+--
+ALTER TABLE `composition`
+  ADD PRIMARY KEY (`refarticle`);
+
+--
+-- Index pour la table `utilisateur`
+--
+ALTER TABLE `utilisateur`
+  ADD PRIMARY KEY (`idu`);
 
 --
 -- AUTO_INCREMENT pour les tables déchargées
@@ -85,13 +169,31 @@ ALTER TABLE `categorie`
 -- AUTO_INCREMENT pour la table `articles`
 --
 ALTER TABLE `articles`
-  MODIFY `ref` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `ref` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT pour la table `avis`
+--
+ALTER TABLE `avis`
+  MODIFY `idavis` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT pour la table `categorie`
 --
 ALTER TABLE `categorie`
-  MODIFY `idcateg` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `idcateg` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT pour la table `commande`
+--
+ALTER TABLE `commande`
+  MODIFY `idcom` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `utilisateur`
+--
+ALTER TABLE `utilisateur`
+  MODIFY `idu` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- Contraintes pour les tables déchargées
