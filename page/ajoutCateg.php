3c84816d18b6d599094097450309b3385bd4bc7c
Arcko-03 <scenic43@live.fr>
2020-05-27 23:14:01 +0200

    Style ajout categ

--- page/ajoutCateg.php
+++ page/ajoutCateg.php
@@ -1,17 +1,15 @@
-<div>
-	
-	<p>ajouter une catégorie</p>
+<div class="police" style="margin-left: 30%; margin-right: 30%;">
+	<center><h1>Ajouter une catégorie</h1></center><br>
 	<form action="index.php?page=control&param=AjoutCategValid" method="post">
 		<?php
 			$lib = "";
 		?>
-		<div>
+		<div class="form-group">
 			<label for="nom">Nom : </label>
-			<input type="text" name="lib" value="<?php echo $lib ?>" required>
+			<input  class="form-control"type="text" name="lib" value="<?php echo $lib ?>" required>
 		</div>
-		<div>
-			<input type="submit" value="Valider">
-		</div>
+		<div class="form-group">
+			<center><button type="submit" class="btn btn-primary">Valider</button></center>
+		</div><br><br><br>
 	</form>
-
 </div>
