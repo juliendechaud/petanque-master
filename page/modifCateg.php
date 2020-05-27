2bc981f46757faa9a359300c67e2988ba185cbf8
Arcko-03 <scenic43@live.fr>
2020-05-27 22:59:34 +0200

    Style modif Categ

--- page/modifCateg.php
+++ page/modifCateg.php
@@ -1,22 +1,23 @@
-<div>
-	
-	<p>modifier une catégorie</p>
-	<form action="index.php?page=control&param=ModifCategValid" method="post">
-		<?php
-			$id = $uneCateg["idcateg"];
-			$lib = $uneCateg["libelle"];
-		?>
-		<div>
-			<label for="id">Id : </label>
-			<input type="text" name="id" value="<?php echo $id ?>" readonly>
-		</div>
-		<div>
-			<label for="lib">Nom : </label>
-			<input type="text" name="lib" value="<?php echo $lib ?>" required>
-		</div>
-		<div>
-			<input type="submit" value="Valider">
-		</div>
-	</form>
-
+<div class="police" style="margin-left: 30%; margin-right: 30%;">
+	<center>
+		<br>
+		<h1>Modifier une catégorie</h1>
+		<form action="index.php?page=control&param=ModifCategValid" method="post">
+			<?php
+				$id = $uneCateg["idcateg"];
+				$lib = $uneCateg["libelle"];
+			?>
+			<div class="form-group">
+				<label for="id">Id : </label>
+				<input class="form-control" type="text" name="id" value="<?php echo $id ?>" readonly>
+			</div>
+			<div class="form-group">
+				<label for="lib">Nom : </label>
+				<input class="form-control" type="text" name="lib" value="<?php echo $lib ?>" required>
+			</div>
+			<div class="form-group">
+				<button type="submit" class="btn btn-primary">Valider</button>
+			</div>
+		</form>
+	</center>
 </div>
