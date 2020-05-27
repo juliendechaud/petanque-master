97e7fbba75b3ea4842261af8f17c4b6044f5ecb3
Arcko-03 <scenic43@live.fr>
2020-05-27 19:24:24 +0200

    Page article

--- page/unArticle.php
+++ page/unArticle.php
@@ -1,11 +1,13 @@
-<?php
-
-	echo "<p>".$laLigne['ref']."</p>";
-	echo "<p>".$laLigne['nom']."</p>";
-	echo "<p>".$laLigne['prix']."</p>";
-	echo "<img src='".$laLigne['photo']."' width='100px'>";
-	echo "<p>".$laLigne['description']."</p>";
-	echo "<p>".$laLigne['categ']."</p>";
-
-?>
-<img src="" width="">
+<div class=police>
+	<center>
+		<?php
+			echo "<img src='".$laLigne['photo']."' width='220px'>";
+			echo "<p style='color: #00bfff; text-decoration: underline;'>".$laLigne['nom']."</p>";
+			echo "<p style='font-size: 20px'>".$laLigne['prix']."€</p>";
+			echo "<p>".$laLigne['description']."</p>";
+			echo "<p>Référence: ".$laLigne['ref']."</p><br>";
+		?>
+		<button type="button" class="btn btn-primary">Ajouter au panier  <img src="img/website/panier.png" height="30px"></button>
+		<br><br>
+	</center>
+</div>
