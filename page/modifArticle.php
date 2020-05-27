91f83b106927f423a1f74574abedbfb288764d78
Arcko-03 <scenic43@live.fr>
2020-05-27 22:55:01 +0200

    Style modif Article

--- page/modifArticle.php
+++ page/modifArticle.php
@@ -1,61 +1,61 @@
-<div>
-	
-	<p>modifier un article</p>
-	<form action="index.php?page=control&param=ModifArticleValid" method="post">
-		<?php
-			$ref = $unArticle["ref"];
-			$nom = $unArticle["nom"];
-			$prix = $unArticle["prix"];
-			$photo = $unArticle["photo"];
-			$descri = $unArticle["description"];
-			$lacateg = $unArticle["categ"];
-		?>
-		<div>
-			<label for="ref">Ref : </label>
-			<input type="text" name="ref" value="<?php echo $ref ?>" readonly>
-		</div>
-		<div>
-			<label for="nom">Nom : </label>
-			<input type="text" name="nom" value="<?php echo $nom ?>" required>
-		</div>
-		<div>
-			<label for="prix">Prix : </label>
-			<input type="text" name="prix" value="<?php echo $prix ?>" required>
-			<label for="prix">€</label>
-		</div>
-		<div>
-			<label for="photo">Photo : </label>
-			<input type="text" name="photo" value="<?php echo $photo ?>" required>
-		</div>
-		<div>
-			<label for="descri">Description : </label>
-			<input type="text" name="descri" value="<?php echo $descri ?>" required>
-		</div>
-		<div>
-			<label for="categ">Catégorie : </label>
-			<select name="categ" required>
-				<option value="">Choisir une catégorie</option>
-				<?php
-					foreach ($lesLignes as $uneCateg)
-					{
-						$categ = $uneCateg['id'];
-						$lib = $uneCateg['lib'];
-
-						if ($categ == $lacateg)
+<div class="police" style="margin-left: 30%; margin-right: 30%;">
+	<center>
+	<br>
+	<h1>Modifier un article</h1>
+		<form action="index.php?page=control&param=ModifArticleValid" method="post">
+			<?php
+				$ref = $unArticle["ref"];
+				$nom = $unArticle["nom"];
+				$prix = $unArticle["prix"];
+				$photo = $unArticle["photo"];
+				$descri = $unArticle["description"];
+				$lacateg = $unArticle["categ"];
+			?>
+			<div class="form-group">
+				<label for="ref">Ref : </label>
+				<input class="form-control" type="text" name="ref" value="<?php echo $ref ?>" readonly>
+			</div>
+			<div class="form-group">
+				<label for="nom">Nom : </label>
+				<input class="form-control" type="text" name="nom" value="<?php echo $nom ?>" required>
+			</div>
+			<div class="form-group">
+				<label for="prix">Prix : </label>
+				<input class="form-control" type="text" name="prix" value="<?php echo $prix ?>" required>
+			</div>
+			<div class="form-group">
+				<label for="photo">Photo : </label>
+				<input class="form-control" type="text" name="photo" value="<?php echo $photo ?>" required>
+			</div>
+			<div class="form-group">
+				<label for="descri">Description : </label>
+				<input class="form-control" type="text" name="descri" value="<?php echo $descri ?>" required>
+			</div>
+			<div class="form-group">
+				<label for="categ">Catégorie : </label>
+				<select class="form-control" name="categ" required>
+					<option value="">Choisir une catégorie</option>
+					<?php
+						foreach ($lesLignes as $uneCateg)
 						{
-							echo "<option value=".$categ." selected>".$lib."</option>";
-						}
-						else
-						{
-							echo "<option value=".$categ.">".$lib."</option>";
-						}
-					}
-				?>
-			</select>
-		</div>
-		<div>
-			<input type="submit" value="Valider">
-		</div>
-	</form>
+							$categ = $uneCateg['id'];
+							$lib = $uneCateg['lib'];
 
+							if ($categ == $lacateg)
+							{
+								echo "<option value=".$categ." selected>".$lib."</option>";
+							}
+							else
+							{
+								echo "<option value=".$categ.">".$lib."</option>";
+							}
+						}
+					?>
+				</select>
+			</div>
+			<div>
+				<button type="submit" class="btn btn-primary">Valider</button><br><br>
+			</div>
+		</form>
+	</center>
 </div>
