276f523fbb3c709631e16fdecdd5898bb7e0d79d
Arcko-03 <scenic43@live.fr>
2020-05-27 22:17:58 +0200

    ERREUR 404

--- page/404.php
+++ page/404.php
@@ -1,3 +1,7 @@
-<h2>Erreur 404</h2>
-<img src="img/website/mrmvZizou.png">
-<p>Oh non pas ça ! pas comme ça !</p>
+<div class="police">
+	<center>
+		<h1 style="color: red; text-decoration: underline;">Erreur 404</h1>
+		<img src="img/website/raté.png" style="margin: 2%">
+		<h1>Raté ! Apprends à tirer la prochaine fois !!!</h1><br>
+	</center>
+</div>
